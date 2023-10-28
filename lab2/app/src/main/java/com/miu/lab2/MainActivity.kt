cad46ee3e8a61a2632144143285966efdeb90f52
Bayasaa <bayarbayasgalan.j@gmail.com>
2023-10-27 21:55:51 -0500

    Done version1 Lab2

---
+++ lab2/app/src/main/java/com/miu/lab2/MainActivity.kt
@@ -0,0 +1,44 @@
+package com.miu.lab2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.miu.lab2.ActivityLab2Binding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLab2Binding
+
+    var arr = arrayOf(
+        "Hamburger", "Pizza",
+        "Mexican", "American", "Chinese"
+    )
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_main)
+        binding = ActivityLab2Binding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.buttonDecide.setOnClickListener{
+//            Log.v("VerboseTag", "======-------+++++++++")
+            val random = java.util.Random()
+            val randomIndex = random.nextInt(arr.size)
+            val randomElement = arr[randomIndex]
+            println("Random element from the list: $randomElement")
+            showMsg("$randomElement")
+        }
+        binding.buttonAddFood.setOnClickListener{
+            val inputText = binding.textInput.text.toString()
+            Log.v("VerboseTag", "======-------+++++++++ $inputText")
+            val newArr = Array(arr.size + 1) { "" }
+            for (i in arr.indices) {
+                newArr[i] = arr[i]
+            }
+            newArr[arr.size] = inputText
+            arr = newArr
+        }
+    }
+    fun showMsg(msg: String) {
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
+        binding.textViewResultValue.text = msg
+    }
+}
