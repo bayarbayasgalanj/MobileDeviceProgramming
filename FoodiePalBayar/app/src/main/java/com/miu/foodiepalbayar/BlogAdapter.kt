d3e86d3ae56df35ef41c8b65cb365ba8e4259d53
Bayasaa <bayarbayasgalan.j@gmail.com>
2023-12-06 14:24:49 -0600

    •	Integrate a social sharing feature for users to share their favorite recipes or blog posts.

--- FoodiePalBayar/app/src/main/java/com/miu/foodiepalbayar/BlogAdapter.kt
+++ FoodiePalBayar/app/src/main/java/com/miu/foodiepalbayar/BlogAdapter.kt
@@ -1,16 +1,18 @@
 package com.miu.foodiepalbayar
-
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
-class BlogAdapter(private val meals: List<Blog>) : RecyclerView.Adapter<BlogAdapter.BlogViewHolder>() {
+class BlogAdapter(private val blogs: List<Blog>) : RecyclerView.Adapter<BlogAdapter.BlogViewHolder>() {
 
     class BlogViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         val textViewTitle: TextView = view.findViewById(R.id.blog_title)
         val textViewArticle: TextView = view.findViewById(R.id.blog_article)
+        val shareButton: ImageButton = view.findViewById(R.id.share_button) // Corrected to ImageButton
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BlogViewHolder {
@@ -19,10 +21,20 @@ class BlogAdapter(private val meals: List<Blog>) : RecyclerView.Adapter<BlogAdap
     }
 
     override fun onBindViewHolder(holder: BlogViewHolder, position: Int) {
-        val currentItem = meals[position]
+        val currentItem = blogs[position]
         holder.textViewTitle.text = currentItem.title
         holder.textViewArticle.text = currentItem.article
+
+        // Set up the share button click listener
+        holder.shareButton.setOnClickListener {
+            val shareIntent = Intent().apply {
+                action = Intent.ACTION_SEND
+                putExtra(Intent.EXTRA_TEXT, "${currentItem.title}\n\n${currentItem.article}")
+                type = "text/plain"
+            }
+            it.context.startActivity(Intent.createChooser(shareIntent, "Share blog via"))
+        }
     }
 
-    override fun getItemCount() = meals.size
+    override fun getItemCount() = blogs.size
 }
